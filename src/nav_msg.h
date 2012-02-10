71ffa7c559e36a42e332d5771b8ec2671cd98b9b
Henry Hallam <hhallam@jobyenergy.com>
2012-02-10 01:03:15 -0800

    Better preamble detection / preliminary TOW extraction

--- src/nav_msg.h
+++ src/nav_msg.h
@@ -3,10 +3,12 @@
 
 #include "int_types.h"
 
+#define NAV_MSG_SUBFRAME_BITS_LEN 12    // Buffer 384 nav bits
+
 typedef struct {
-  u32 subframe_bits[12];
+  u32 subframe_bits[NAV_MSG_SUBFRAME_BITS_LEN];
   u16 subframe_bit_index;
-  s16 subframe_preamble_index; // 0 = no preamble found, +x = preamble begins at bit index (x-1), -x = inverse preamble begins at (1-x)
+  s16 subframe_start_index; // 0 = no preamble found, +x = preamble begins at bit index (x-1), -x = inverse preamble begins at (1-x)
   u8 bit_phase;
   u8 bit_phase_ref;
   u8 bit_phase_count;
