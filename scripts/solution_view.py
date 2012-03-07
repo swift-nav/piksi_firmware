85bf85a94091683c95675056ae85e7a779eea94f
Fergus Noble <fergusnoble@gmail.com>
2012-03-06 18:24:31 -0800

    Updates to solution view to show means etc.

--- scripts/solution_view.py
+++ scripts/solution_view.py
@@ -1,5 +1,5 @@
 from enthought.traits.api import Str, Instance, Dict, HasTraits, Array, Float, on_trait_change, List, Int, Button
-from enthought.traits.ui.api import Item, View, HGroup
+from enthought.traits.ui.api import Item, View, HGroup, VGroup
 
 from enthought.chaco.api import BarPlot, ArrayDataSource, DataRange1D, LinearMapper, OverlayPlotContainer, LabelAxis, PlotAxis, ArrayPlotData, Plot
 from enthought.enable.api import ComponentEditor, Component
@@ -27,8 +27,13 @@ class SolutionView(HasTraits):
   pos_llh = Array(dtype=float, shape=(3,))
   pos_ned = Array(dtype=float, shape=(3,))
   ns = List()
+  mean_ns = Float()
   es = List()
+  mean_es = Float()
   ds = List()
+  mean_ds = Float()
+  mean_len = Int(50)
+  mean_dist = Float()
   n_used = Int()
   dist = Float()
 
@@ -53,7 +58,17 @@ class SolutionView(HasTraits):
     HGroup(
       Item('pos_llh'),
       Item('pos_ned'),
-      Item('dist'),
+      VGroup(
+        Item('dist'),
+        Item('mean_len'),
+        Item('mean_dist'),
+      ),
+      VGroup(
+        Item('mean_ns'),
+        Item('mean_es'),
+        Item('mean_ds'),
+      ),
+      Item('n_used')
     ),
     Item(
       'plot',
@@ -93,7 +108,9 @@ class SolutionView(HasTraits):
     t = range(len(pred_prs[0]))
     self.pr_plot_data.set_data('t', t)
     for n in range(TRACK_N_CHANNELS):
-      self.pr_plot_data.set_data('pred_prs'+str(n), prs[n]-pred_prs[n])
+      err = prs[n]-pred_prs[n]
+      #err = err - (sum(err)/len(err))
+      self.pr_plot_data.set_data('pred_prs'+str(n), err)
 
   def solution_callback(self, data):
     soln = struct.unpack('<3d3d3d3d3d7ddBB', data)
@@ -102,10 +119,10 @@ class SolutionView(HasTraits):
     self.pos_ned = soln[6:9]
     vel_xyz = soln[9:12]
     vel_ned = soln[12:15]
-    err_cov = soln[15:21]
-    time = soln[21]
-    soln_valid = soln[22]
-    self.n_used = soln[23]
+    err_cov = soln[15:22]
+    time = soln[22]
+    soln_valid = soln[23]
+    self.n_used = soln[24]
 
     self.dist = math.sqrt(self.pos_ned[0]**2 + self.pos_ned[1]**2 + self.pos_ned[2]**2)
 
@@ -115,10 +132,17 @@ class SolutionView(HasTraits):
       self.ds.append(self.pos_ned[2])
     else:
       print "Whacky solution detected!"
+    
+    self.mean_ns = sum(self.ns[:self.mean_len])/len(self.ns[:self.mean_len])
+    self.mean_es = sum(self.es[:self.mean_len])/len(self.es[:self.mean_len])
+    self.mean_ds = sum(self.ds[:self.mean_len])/len(self.ds[:self.mean_len])
+    self.mean_dist = math.sqrt(self.mean_ns**2 + self.mean_es**2 + self.mean_ds**2)
 
     self.plot_data.set_data('n', self.ns)
     self.plot_data.set_data('e', self.es)
     self.plot_data.set_data('h', self.ds)
+    self.plot_data.set_data('ref_n', [0.0, self.mean_ns])
+    self.plot_data.set_data('ref_e', [0.0, self.mean_es])
     t = range(len(self.ds))
     self.plot_data.set_data('t', t)
 
@@ -134,9 +158,16 @@ class SolutionView(HasTraits):
     self.link.add_callback(MSG_SOLUTION_PRS, self.prs_callback)
     self.link.add_callback(MSG_SOLUTION_PRED_PRS, self.pred_prs_callback)
 
-    self.plot_data = ArrayPlotData(n=[0.0], e=[0.0], h=[0.0], t=[0.0])
+    self.plot_data = ArrayPlotData(n=[0.0], e=[0.0], h=[0.0], t=[0.0], ref_n=[0.0], ref_e=[0.0])
     self.plot = Plot(self.plot_data) #, auto_colors=colours_list)
-    self.plot.plot(('n', 'e'), type='line', color='blue')
+    self.plot.plot(('n', 'e'), type='scatter', color='blue', marker='plus')
+    self.plot.plot(('ref_n', 'ref_e'),
+        type='scatter',
+        color='red',
+        marker='cross',
+        marker_size=10,
+        line_width=1.5
+    )
     #self.plot.plot(('h', 'e'), type='line', color='red')
 
     self.pr_plot_data = ArrayPlotData(t=[0.0])
@@ -145,7 +176,7 @@ class SolutionView(HasTraits):
     #self.pr_plot.value_range.low_setting = 0.0
     for n in range(TRACK_N_CHANNELS):
       self.pr_plot_data.set_data('prs'+str(n), [0.0])
-      self.pr_plot.plot(('t', 'prs'+str(n)), type='scatter', color='auto')
+      self.pr_plot.plot(('t', 'prs'+str(n)), type='line', color='auto')
       #self.pr_plot_data.set_data('pred_prs'+str(n), [0.0])
       #self.pr_plot.plot(('t', 'pred_prs'+str(n)), type='line', color='auto')
 
