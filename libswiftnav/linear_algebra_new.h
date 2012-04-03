95980436fd8ddb75c272fb64fad462d29e15293c
Fergus Noble <fergusnoble@gmail.com>
2012-04-02 17:03:28 -0700

    Update license comment blocks for linear_algebra_new.

--- libswiftnav/linear_algebra_new.h
+++ libswiftnav/linear_algebra_new.h
@@ -1,7 +1,12 @@
 /*
- * linear_algebra_new.h
- * Linear algebra routines for SwiftLib
- * Copyright (c) 2012, Matt Peddie <peddie@alum.mit.edu>
+ * Copyright (C) 2012 Matt Peddie <peddie@alum.mit.edu>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
  */
 
 #ifndef SWIFTLIB_COORD_SYSTEM_H
