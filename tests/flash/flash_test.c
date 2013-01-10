ab68acc80260e262a5fc5e1196f3b8c02546ca07
Colin Beighley <colinbeighley@gmail.com>
2013-01-09 16:06:57 -0800

    Firmware for loading new configuration onto FPGA's flash via flash.py, doesn't depend on having any FPGA configuration

---
+++ tests/flash/flash_test.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2011 Fergus Noble <fergusnoble@gmail.com>
+ * Copyright (C) 2012 Colin Beighley <colin@swift-nav.com>
+ *
+ */
+
+#include <stdio.h>
+#include <libopencm3/stm32/f2/rcc.h>
+#include <libopencm3/stm32/f2/flash.h>
+#include <libopencm3/stm32/f2/gpio.h>
+
+#include "main.h"
+#include "debug.h"
+#include "swift_nap_io.h"
+#include "acq.h"
+#include "hw/spi.h"
+#include "hw/leds.h"
+#include "hw/m25_flash.h"
+
+int main(void)
+{
+
+  led_setup();
+  led_on(LED_GREEN);
+  led_on(LED_RED);
+
+  //Hold FPGA re-configuration line low while we flash the flash
+  RCC_AHB1ENR |= RCC_AHB1ENR_IOPCEN;
+	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
+  gpio_clear(GPIOC, GPIO12);
+
+  spi_setup();
+  m25_setup();
+  debug_setup();
+
+  for (u32 i = 0; i < 600000; i++)
+    __asm__("nop");
+
+  printf("\n\nFirmware info - git: " GIT_VERSION ", built: " __DATE__ " " __TIME__ "\n");
+  printf("--- M25 FLASH TEST ---\n");
+
+  while (1) {
+    for (u32 i = 0; i < 10000; i++) 
+      __asm__("nop");
+    DO_EVERY(20,
+      led_toggle(LED_GREEN);
+      led_toggle(LED_RED);
+    );
+    debug_process_messages();
+  }
+
+  return 0;
+}
