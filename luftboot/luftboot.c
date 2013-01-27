6b316dd1c1789e080d7ac5a3a2f5f795baf77304
Colin Beighley <colinbeighley@gmail.com>
2013-01-26 17:31:59 -0800

    UART send/receive working

--- luftboot/luftboot.c
+++ luftboot/luftboot.c
@@ -24,6 +24,7 @@
 #include <libopencm3/stm32/f4/gpio.h>
 #include <libopencm3/stm32/f4/flash.h>
 #include <libopencm3/stm32/f4/scb.h>
+#include <libopencm3/stm32/f4/usart.h>
 
 #include "main.h"
 #include "hw/leds.h"
@@ -31,23 +32,48 @@
 #define APP_ADDRESS	0x08002000
 
 /* Commands sent with wBlockNum == 0 as per ST implementation. */
-#define CMD_SETADDR	0x21 
-#define CMD_ERASE	0x41 
+//#define CMD_SETADDR	0x21 
+//#define CMD_ERASE	0x41 
 
-//static struct {
-//	u8 buf[sizeof(usbd_control_buffer)];
-//	u16 len;
-//	u32 addr;
-//	u16 blocknum;
-//} prog;
+void clock_setup(void)
+{
+	/* Enable GPIOD clock for LED & USARTs. */
+	rcc_peripheral_enable_clock(&RCC_AHB1ENR, RCC_AHB1ENR_IOPCEN);
+
+	/* Enable clocks for USART6. */
+	rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_USART6EN);
+}
+
+void usart_setup(void)
+{
+  /* Setup USART6 pins */
+  gpio_mode_setup(GPIOC, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO6|GPIO7);
+	gpio_set_af(GPIOC, GPIO_AF8, GPIO6|GPIO7);
+
+	/* Setup USART6 parameters. */
+	usart_set_baudrate(USART6, 230400);
+	usart_set_databits(USART6, 8);
+	usart_set_stopbits(USART6, USART_STOPBITS_1);
+	usart_set_mode(USART6, USART_MODE_TX_RX);
+	usart_set_parity(USART6, USART_PARITY_NONE);
+	usart_set_flow_control(USART6, USART_FLOWCONTROL_NONE);
+
+	/* Finally enable the USART. */
+	usart_enable(USART6);
+}
+
 
 int main(void)
 {
-	/* Enable clock for the "force bootloader" pin bank and check for it */
-//	rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPCEN);
-//	gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_PULL_UPDOWN, GPIO0);
-//	gpio_clear(GPIOC, GPIO0);
-//	if(gpio_get(GPIOC, GPIO0)) {
+  /* Setup clock for UART and LEDs */
+  clock_setup();
+  /* Setup UART6 to receive data from flash */
+  usart_setup();
+
+  led_setup();
+  led_on(LED_GREEN);
+  led_on(LED_RED);
+
   /* Boot the application if it's valid */
   if ((*(volatile u32*)APP_ADDRESS & 0x2FFE0000) == 0x20000000) {
     /* Set vector table base address */
@@ -58,29 +84,24 @@ int main(void)
     /* Jump to application */
     (*(void(**)())(APP_ADDRESS + 4))();
   }
-//	}
 
-//	rcc_clock_setup_in_hse_12mhz_out_72mhz();
-
-//	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, 
-//			GPIO_CNF_OUTPUT_PUSHPULL, GPIO5);
-//	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, 
-//			GPIO_CNF_OUTPUT_PUSHPULL, GPIO2);
-//	gpio_toggle(GPIOC, GPIO2); /* LED2 on/off */
-  
-	
 //	systick_set_clocksource(STK_CTRL_CLKSOURCE_AHB_DIV8); 
 //	systick_set_reload(900000);
 //	systick_interrupt_enable();
 //	systick_counter_enable();
 
-  led_setup();
+  u16 uart_data;
 	
 	while (1){
-    DO_EVERY(100000,
+    /* Echo if we have a character ready on the UART */
+    if (USART_SR(USART6) & USART_SR_RXNE) {
+      uart_data = usart_recv(USART6);
+      usart_send_blocking(USART6, uart_data);
+      usart_send_blocking(USART6, uart_data);
+    }
+    DO_EVERY(500000,
       led_toggle(LED_GREEN);
       led_toggle(LED_RED);
     );
-    __asm__("nop");
   }
 }
