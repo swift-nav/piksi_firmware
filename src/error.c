60182908c7f29558d45dc3961c5612e571f67237
Colin Beighley <colinbeighley@gmail.com>
2013-05-22 15:51:50 -0700

    Corrected CRC for screaming_death and made screaming_death more similar to speaking_death

--- src/error.c
+++ src/error.c
@@ -19,44 +19,46 @@
 #include <libopencm3/stm32/f4/dma.h>
 
 #include "hw/leds.h"
+#include "hw/usart.h"
 
 #include "debug.h"
 #include "error.h"
 
 /** Last resort, low-level error function.
  * Halts the program while continually sending a non-descript error message in
- * debug message format, in a way that should get the message through to the
- * Python console even if it's interrupting another transmission.
+ * debug message format to the FTDI USART, in a way that should get the message  * through to the Python console even if it's interrupting another transmission.
  */
 void screaming_death(void)
 {
-  /* Disable ALL interrupts. */
-  __asm__("CPSID if;");
+  __asm__("CPSID if;");           /* Disable ALL interrupts. */
+  DMA2_S7CR = 0;                  /* Disable USART TX DMA */
+  USART6_CR3 &= ~USART_CR3_DMAT;  /* Disable USART DMA */
 
-  led_on(LED_RED);
+  #define ERR_MSG_N = 7           /* Length of error message */
 
-  static char err_msg[13] = {DEBUG_MAGIC_1, DEBUG_MAGIC_2,
-                             MSG_PRINT, 13,
-                             'E', 'R', 'R', 'O', 'R', '!', '\n',
-                             [11] = 0xAD, /* hard coded CRC for this message */
-                             [12] = 0x28};
+  static char err_msg[ERR_MSG_N+6] = {DEBUG_MAGIC_1, DEBUG_MAGIC_2,
+                                      MSG_PRINT, ERR_MSG_N,
+                                      'E', 'R', 'R', 'O', 'R', '!', '\n',
+                                      /* hard coded CRC for this message */
+                                      [ERR_MSG_N+4] = 0x3c,
+                                      [ERR_MSG_N+5] = 0x54};
   u8 i = 0;
-
-  while(1) {
-    while(error_string[i] != 0) {
-      usart_send_blocking(USART6, error_string[i]);
-      i++;
+  while (1) {
+    while (!(USART6_SR & USART_SR_TXE));
+    USART6_DR = err_msg[i];
+    if (++i == (ERR_MSG_N + 6)) {
+      i = 0;
+      led_toggle(LED_RED);
+      for (u32 d = 0; d < 5000000; d++)
+        __asm__("nop");
     }
-
-    for (u32 i = 0; i < 600000; i++)
-      __asm__("nop");
   }
 };
 
 /** Last resort, low-level error message function.
  * Halts the program while continually sending a fixed error message in debug
- * message format, in a way that should get the message through to the Python
- * console even if it's interrupting another transmission.
+ * message format to the FTDI USART, in a way that should get the message
+ * through to the Python console even if it's interrupting another transmission.
  *
  * \param msg A pointer to an array of chars containing the error message.
  */
@@ -82,12 +84,13 @@ void speaking_death(char *msg)
   while (*msg && i < ERR_MSG_N)   /* Don't want to use C library memcpy */
     err_msg[11+(i++)] = *msg++;
 
-  /* Insert CRC into last two indices */
+  /* Insert CRC */
   u16 crc = crc16_ccitt(&err_msg[2], 2, 0);
   crc = crc16_ccitt(&err_msg[4], ERR_MSG_N, crc);
   err_msg[ERR_MSG_N+4] = (crc >> 8) & 0xFF;
   err_msg[ERR_MSG_N+5] = crc & 0xFF;
 
+  /* Continuously send error message */
   i=0;
   while (1) {
     while (!(USART6_SR & USART_SR_TXE));
