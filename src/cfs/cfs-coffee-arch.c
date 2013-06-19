a9ab327c12faeffc52c3d47c6998d1ce5c4a9977
Fergus Noble <fergusnoble@gmail.com>
2013-06-19 00:08:02 -0700

    Documenting local coffee filesystem configuration.

--- src/cfs/cfs-coffee-arch.c
+++ src/cfs/cfs-coffee-arch.c
@@ -14,12 +14,30 @@
 
 #include "cfs/cfs-coffee-arch.h"
 
+/** \addtogroup cfs
+ * \{
+ */
+
+/** \defgroup cfs_arch Local Coffee configuration
+ * \{
+ */
+
+/** Read from the Coffee filesystem area in STM flash.
+ * \param buf Pointer to a buffer where the read values will be stored.
+ * \param size Number of bytes to read.
+ * \param offset Offset into the filesystem area to read from.
+ */
 void coffee_read(u8* buf, u32 size, u32 offset)
 {
   for (u32 i=0; i<size; i++)
     buf[i] = ~((u8*)(COFFEE_START+offset))[i];
 }
 
+/** Write to the Coffee filesystem area in STM flash.
+ * \param buf Pointer to a buffer containing the values to be written.
+ * \param size Number of bytes to write.
+ * \param offset Offset into the filesystem area to write to.
+ */
 void coffee_write(u8* buf, u32 size, u32 offset)
 {
   flash_unlock();
@@ -30,6 +48,10 @@ void coffee_write(u8* buf, u32 size, u32 offset)
   flash_lock();
 }
 
+/** Erase sector of the Coffee filesystem area in STM flash.
+ * Erases flash sectors of size \ref COFFEE_SECTOR_SIZE.
+ * \param sector Sector number to erase, starting from zero.
+ */
 void coffee_erase(u8 sector)
 {
   flash_unlock();
@@ -37,4 +59,7 @@ void coffee_erase(u8 sector)
   flash_lock();
 }
 
+/** \} */
+
+/** \} */
 
