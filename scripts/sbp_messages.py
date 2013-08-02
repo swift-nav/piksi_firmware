a3e199237484b78afc9bd70b9e2237c5bfb57394
Fergus Noble <fergusnoble@gmail.com>
2013-08-01 21:28:48 -0700

    Update regex so python finds SBP ids with numbers in their name.

--- scripts/sbp_messages.py
+++ scripts/sbp_messages.py
@@ -10,7 +10,7 @@ header_file = os.path.join(
 with open(header_file, 'r') as f:
   sbph = f.read()
 
-msgs = re.findall('^\s*#define\s+MSG_([A-Z_]+)\s+0x([A-F0-9][A-F0-9])',
+msgs = re.findall('^\s*#define\s+MSG_([A-Z0-9_]+)\s+0x([A-F0-9][A-F0-9])',
                   sbph, re.MULTILINE)
 
 for name, id in msgs:
