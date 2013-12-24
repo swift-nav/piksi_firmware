027624114f9153a23e0986204a49e1e9a523a98b
Colin Beighley <colinbeighley@gmail.com>
2013-12-24 14:59:28 -0800

    Added comment about changing NAP_HASH status values

--- src/board/nap/nap_common.h
+++ src/board/nap/nap_common.h
@@ -31,6 +31,7 @@
 #define NAP_REG_DECEASED_COW        0xFF
 
 /* Status of NAP authentication hash comparison. */
+/* TODO: change NAP_HASH_MATCH to non 0x00/0xFF value so it is more reliable */
 #define NAP_HASH_MATCH              0
 #define NAP_HASH_MISMATCH           1
 #define NAP_HASH_NOTREADY           2
