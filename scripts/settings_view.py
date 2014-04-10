7d69c814a394465fa2f04a41f16258532f294b97
Fergus Noble <fergusnoble@gmail.com>
2014-04-10 03:51:57 -0700

    First cut of settings_view with write support.

--- scripts/settings_view.py
+++ scripts/settings_view.py
@@ -9,7 +9,7 @@
 # EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
 
-from traits.api import Instance, Dict, HasTraits, Array, Float, on_trait_change, List, Int, Button, Bool
+from traits.api import Instance, Dict, HasTraits, Array, Float, on_trait_change, List, Int, Button, Bool, Str, Color, Constant, Font, Undefined
 from traitsui.api import Item, View, HGroup, VGroup, ArrayEditor, HSplit, TabularEditor
 from traitsui.tabular_adapter import TabularAdapter
 
@@ -21,28 +21,62 @@ import datetime
 
 import sbp_piksi as ids
 
+
+
+class SettingBase(HasTraits):
+  name = Str()
+  value = Str(Undefined)
+
+class Setting(SettingBase):
+  section = Str()
+
+  def __init__(self, name, section, value, link):
+    self.name = name
+    self.section = section
+    self.value = value
+    self.link = link
+
+  def _value_changed(self, name, old, new):
+    if (old != new and
+        old is not Undefined and
+        new is not Undefined):
+      print old, new
+      if type(self.value) == unicode:
+        self.value = self.value.encode('ascii', 'replace')
+      print (self.section, self.name, self.value)
+      print repr('%s\0%s\0%s' % (self.section, self.name, self.value))
+      self.link.send_message(ids.SETTINGS, '%s\0%s\0%s' % (self.section, self.name, self.value))
+
+class SectionHeading(SettingBase):
+  value = Constant('')
+
+  def __init__(self, name):
+    self.name = name
+
 class SimpleAdapter(TabularAdapter):
-  columns = [('Setting', 0), ('Value',  1)]
+  columns = [('Name', 'name'), ('Value',  'value')]
+  SectionHeading_bg_color = Color(0xE0E0E0)
+  SectionHeading_font = Font('bold')
+  SectionHeading_can_edit = Bool(False)
+  #Setting_name_can_edit = Bool(False)
+  #Setting_value_can_edit = Bool(True)
 
 class SettingsView(HasTraits):
 
   settings_read_button = Button(label='Read settings')
   settings_save_button = Button(label='Save settings to config file')
 
-  settings_table = List()
+  settings_list = List(SettingBase)
 
   traits_view = View(
     HSplit(
-      Item('settings_table', editor = TabularEditor(adapter=SimpleAdapter()), show_label=False, width=0.6),
+      Item('settings_list', editor = TabularEditor(adapter=SimpleAdapter(), editable_labels=False), show_label=False, width=0.6),
       VGroup(
         Item('settings_read_button', show_label=False),
         Item('settings_save_button', show_label=False)
       ),
     )
   )
-
-  def _settings_table_changed(self, old, new):
-    print old, new
 
   ##Simulator buttons
   def _settings_read_button_fired(self):
@@ -58,24 +92,33 @@ class SettingsView(HasTraits):
   ##Callbacks for receiving messages
 
   def settings_read_callback(self, data):
-    print repr(data)
     section, setting, value, _ = data.split('\0')
-    key = section + '.' + setting
+
     if self.discover_state:
-      if self.settings.has_key(key):
+      if self.settings.has_key(section) and self.settings[section].has_key(setting):
         self.discover_state = False
         print self.settings
-        self.settings_table = []
-        keys = self.settings.keys()
-        keys.sort()
-        for k in keys:
-          self.settings_table.append([k, self.settings[k]])
+        self.settings_list = []
+
+        sections = sorted(self.settings.keys())
+
+        for sec in sections:
+          self.settings_list.append(SectionHeading(sec))
+          for setting in sorted(self.settings[sec].keys()):
+            self.settings_list.append(self.settings[sec][setting])
+
       else:
-        print "Found setting: %s = %s" % (key, value)
+        print "Found setting: %s.%s = %s" % (section, setting, value)
+        if not self.settings.has_key(section):
+          self.settings[section] = {}
+        self.settings[section][setting] = Setting(setting, section, value, link=self.link)
         self.link.send_message(ids.SETTINGS, "")
+
     else:
-      print "Setting updated: %s = %s" % (key, value)
-    self.settings[section + '.' + setting] = value
+      print "Setting updated: %s.%s = %s" % (section, setting, value)
+      # Hack to prevent an infinite loop of setting settings
+      self.settings[section][setting].value = Undefined
+      self.settings[section][setting].value = value
 
   def __init__(self, link):
     super(SettingsView, self).__init__()
@@ -86,6 +129,6 @@ class SettingsView(HasTraits):
     self.link.add_callback(ids.SETTINGS, self.settings_read_callback)
 
     self.python_console_cmds = {
-      'sim': self
+      'settings': self
     }
 
