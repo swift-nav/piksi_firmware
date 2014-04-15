6e4b244c530cfd890d30339d1696deaf00f9a90b
Fergus Noble <fergusnoble@gmail.com>
2014-04-15 04:52:50 -0700

    Suppress TOW_ms debugging output unless there is an error condition.

--- src/track.c
+++ src/track.c
@@ -227,7 +227,9 @@ void tracking_channel_update(u8 channel)
       s32 TOW_ms = nav_msg_update(&chan->nav_msg, cs[1].I);
 
       if (TOW_ms > 0 && chan->TOW_ms != TOW_ms) {
-        printf("PRN %d TOW mismatch: %u, %u\n",(int)chan->prn + 1, (unsigned int)chan->TOW_ms, (unsigned int)TOW_ms);
+        if (chan->TOW_ms > 0) {
+          printf("PRN %d TOW mismatch: %ld, %u\n",(int)chan->prn + 1, chan->TOW_ms, (unsigned int)TOW_ms);
+        }
         chan->TOW_ms = TOW_ms;
       }
 
