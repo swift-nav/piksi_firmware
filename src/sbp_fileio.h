2cd5248faa60dd368604bcaad30c6b37feb379e6
Gareth McMullin <gareth@blacksphere.co.nz>
2014-06-05 12:15:11 -0700

    Expose filesystem access over SBP.

---
+++ src/sbp_fileio.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2012-2014 Swift Navigation Inc.
+ * Contact: Fergus Noble <fergus@swift-nav.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef SWIFTNAV_SBP_FILEIO_H
+#define SWIFTNAV_SBP_FILEIO_H
+
+void sbp_fileio_setup(void);
+
+#endif
+
