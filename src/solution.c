f09a63e573081b0d708c7655a9175b7d632fea0f
Ian Horn <horn.imh@gmail.com>
2014-09-18 15:29:21 -0700

    removed extraneous prints

--- src/solution.c
+++ src/solution.c
@@ -330,54 +330,54 @@ void obs_callback(u16 sender_id, u8 len, u8 msg[], void* context)
 
         (void) old_num_base_obss; //potentially used here for online corrections
         /* Loop over base_obss_raw.nm and base_obss.nm and check if a PRN is present in both. */
-        u8 i, j, n = 0;
-        double first_pseudorange_err;
-        double first_carr_phase_err;
-        // printf("\t\tcomparing %u old to %u new\n", old_num_base_obss, base_obss_raw.n);
-        for (i=0, j=0; i<base_obss_raw.n && j<old_num_base_obss; i++, j++) {
-          if (base_obss_raw.nm[i].prn < base_obss.nm[j].prn)
-            j--;
-          else if (base_obss_raw.nm[i].prn > base_obss.nm[j].prn)
-            i--;
-          else {
-            double dist_diff = base_obss.sat_dists[j] - base_obss_raw.sat_dists[i];
-            double pseudorange_err = base_obss_raw.nm[i].pseudorange - base_obss.nm[j].pseudorange
-                                                            + dist_diff;
-            double carrier_phase_err = base_obss_raw.nm[i].carrier_phase - base_obss.nm[j].carrier_phase
-                                                            - dist_diff / GPS_L1_LAMBDA;
-            double pseudorange_diff = base_obss_raw.nm[i].pseudorange - base_obss.nm[j].pseudorange;
-            double carrier_phase_diff = base_obss_raw.nm[i].carrier_phase - base_obss.nm[j].carrier_phase;
+        // u8 i, j, n = 0;
+        // double first_pseudorange_err;
+        // double first_carr_phase_err;
+        // // printf("\t\tcomparing %u old to %u new\n", old_num_base_obss, base_obss_raw.n);
+        // for (i=0, j=0; i<base_obss_raw.n && j<old_num_base_obss; i++, j++) {
+        //   if (base_obss_raw.nm[i].prn < base_obss.nm[j].prn)
+        //     j--;
+        //   else if (base_obss_raw.nm[i].prn > base_obss.nm[j].prn)
+        //     i--;
+        //   else {
+        //     double dist_diff = base_obss.sat_dists[j] - base_obss_raw.sat_dists[i];
+        //     double pseudorange_err = base_obss_raw.nm[i].pseudorange - base_obss.nm[j].pseudorange
+        //                                                     + dist_diff;
+        //     double carrier_phase_err = base_obss_raw.nm[i].carrier_phase - base_obss.nm[j].carrier_phase
+        //                                                     - dist_diff / GPS_L1_LAMBDA;
+        //     double pseudorange_diff = base_obss_raw.nm[i].pseudorange - base_obss.nm[j].pseudorange;
+        //     double carrier_phase_diff = base_obss_raw.nm[i].carrier_phase - base_obss.nm[j].carrier_phase;
 
-            if (n == 0) {
-              first_pseudorange_err = pseudorange_err;
-              first_carr_phase_err = carrier_phase_err;
-            }
-            (void) first_pseudorange_err;
-            (void) first_carr_phase_err;
+        //     if (n == 0) {
+        //       first_pseudorange_err = pseudorange_err;
+        //       first_carr_phase_err = carrier_phase_err;
+        //     }
+        //     (void) first_pseudorange_err;
+        //     (void) first_carr_phase_err;
 
-            printf("   base_obss (prn %u) psuedorange_adj     = %f\n", base_obss_raw.nm[i].prn,
-                                pseudorange_diff);
-            printf("             (prn %u) carrier_phase_adj   = %f\n", base_obss_raw.nm[i].prn,
-                                carrier_phase_diff);
-            printf("             (prn %u) dist_diff           = %f\n", base_obss_raw.nm[i].prn,
-                                dist_diff);
-            // printf("\t\t\t(prn %u) psuedorange_err      = %f\n", base_obss_raw.nm[i].prn,
-            //                 pseudorange_err);
-            // printf("\t\t\t(prn %u) carrier_phase_err    = %f\n", base_obss_raw.nm[i].prn,
-            //                 carrier_phase_err);
-            // printf("\t\t\t(prn %u) sd_psuedorange_err   = %f\n", base_obss_raw.nm[i].prn,
-            //                 pseudorange_err - first_pseudorange_err);
-            printf("             (prn %u) sd_carrier_phase_err = %f\n", base_obss_raw.nm[i].prn,
-                            carrier_phase_err - first_carr_phase_err);
-            // printf("\t\t\t(prn %u) psuedorange_diff     = %f\n", base_obss_raw.nm[i].prn,
-            //                 pseudorange_diff);
-            // printf("\t\t\t(prn %u) carrier_phase_diff   = %f\n", base_obss_raw.nm[i].prn,
-            //                 carrier_phase_diff);
-            // printf("\t\t\t(prn %u) dist_diff            = %f\n\n", base_obss_raw.nm[i].prn,
-            //                 dist_diff);
-            n++;
-          }
-        }
+        //     printf("   base_obss (prn %u) psuedorange_adj     = %f\n", base_obss_raw.nm[i].prn,
+        //                         pseudorange_diff);
+        //     printf("             (prn %u) carrier_phase_adj   = %f\n", base_obss_raw.nm[i].prn,
+        //                         carrier_phase_diff);
+        //     printf("             (prn %u) dist_diff           = %f\n", base_obss_raw.nm[i].prn,
+        //                         dist_diff);
+        //     // printf("\t\t\t(prn %u) psuedorange_err      = %f\n", base_obss_raw.nm[i].prn,
+        //     //                 pseudorange_err);
+        //     // printf("\t\t\t(prn %u) carrier_phase_err    = %f\n", base_obss_raw.nm[i].prn,
+        //     //                 carrier_phase_err);
+        //     // printf("\t\t\t(prn %u) sd_psuedorange_err   = %f\n", base_obss_raw.nm[i].prn,
+        //     //                 pseudorange_err - first_pseudorange_err);
+        //     printf("             (prn %u) sd_carrier_phase_err = %f\n", base_obss_raw.nm[i].prn,
+        //                     carrier_phase_err - first_carr_phase_err);
+        //     // printf("\t\t\t(prn %u) psuedorange_diff     = %f\n", base_obss_raw.nm[i].prn,
+        //     //                 pseudorange_diff);
+        //     // printf("\t\t\t(prn %u) carrier_phase_diff   = %f\n", base_obss_raw.nm[i].prn,
+        //     //                 carrier_phase_diff);
+        //     // printf("\t\t\t(prn %u) dist_diff            = %f\n\n", base_obss_raw.nm[i].prn,
+        //     //                 dist_diff);
+        //     n++;
+        //   }
+        // }
         // printf("\t\tcopying obss over from raw\n\n");
         memcpy(&base_obss, &base_obss_raw, sizeof(obss_t));
         old_num_base_obss = base_obss_raw.n;
@@ -556,20 +556,20 @@ static msg_t solution_thread(void *arg)
               double prop_baseline[3];
               //TODO get a version that knows whether the baseline is using the integer ambs
               u8 num_sds_used;
-              printf("starting low-latency solution\n");
+              // printf("starting low-latency solution\n");
               s8 ll_err_code = dgnss_low_latency_baseline(num_sdiffs, sdiffs,
                       position_solution.pos_ecef, &num_sds_used, prop_baseline);
-              printf("finished low-latency solution\n");
+              // printf("finished low-latency solution\n");
               if (ll_err_code != -1) {
-                printf("starting sending low-latency solution\n");
+                // printf("starting sending low-latency solution\n");
                 solution_send_baseline(&position_solution.time,
                                        num_sds_used, prop_baseline,
                                        position_solution.pos_ecef,
                                        (ll_err_code == 1) ? 1 : 0);
-                printf("finished sending low-latency solution\n");
+                // printf("finished sending low-latency solution\n");
               }
               else {
-                printf("could't compute low-latency solution\n");
+                // printf("could't compute low-latency solution\n");
               }
             }
 
