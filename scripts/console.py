4b7cd00c7522669f4de8b6294a66d005757fd7dd
Fergus Noble <fergusnoble@gmail.com>
2014-10-10 17:54:28 -0700

    CONSOLE: Insert settings view environment back into python shell

--- scripts/console.py
+++ scripts/console.py
@@ -194,6 +194,7 @@ class SwiftConsole(HasTraits):
       self.python_console_env.update(self.observation_view.python_console_cmds)
       self.python_console_env.update(self.system_monitor_view.python_console_cmds)
       self.python_console_env.update(self.update_view.python_console_cmds)
+      self.python_console_env.update(self.settings_view.python_console_cmds)
     except:
       import traceback
       traceback.print_exc()
