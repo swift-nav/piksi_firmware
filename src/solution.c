17533ceca7a60744d41b1aa6b528edcaf31d6680
Scott Kovach <dskovach@gmail.com>
2014-10-22 12:54:24 -0700

    PR comments

--- src/solution.c
+++ src/solution.c
@@ -678,7 +678,7 @@ void process_matched_obs(u8 n_sds, gps_time_t *t, sdiff_t *sds)
         flags = dgnss_fixed_baseline2(n_sds, sds, position_solution.pos_ecef,
                                       &num_used, b);
         if (flags == 0) {
-          // Fixed baseline could not be calculated
+          /* Fixed baseline could not be calculated. */
           dgnss_new_float_baseline(n_sds, sds,
               position_solution.pos_ecef, &num_used, b);
         }
