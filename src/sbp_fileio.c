9f831b6fbd23124559b5fae0b617547cbff3782a
Mark Fine <mark.fine@gmail.com>
2015-06-23 17:19:51 -0700

    update to new file_io messages.

--- src/sbp_fileio.c
+++ src/sbp_fileio.c
@@ -31,13 +31,13 @@ void sbp_fileio_setup(void)
 {
   static sbp_msg_callbacks_node_t read_node;
   sbp_register_cbk(
-    SBP_MSG_FILEIO_READ,
+    SBP_MSG_FILEIO_READ_REQUEST,
     &read_cb,
     &read_node
   );
   static sbp_msg_callbacks_node_t read_dir_node;
   sbp_register_cbk(
-    SBP_MSG_FILEIO_READ_DIR,
+    SBP_MSG_FILEIO_READ_DIR_REQUEST,
     &read_dir_cb,
     &read_dir_node
   );
@@ -49,18 +49,18 @@ void sbp_fileio_setup(void)
   );
   static sbp_msg_callbacks_node_t write_node;
   sbp_register_cbk(
-    SBP_MSG_FILEIO_WRITE,
+    SBP_MSG_FILEIO_WRITE_REQUEST,
     &write_cb,
     &write_node
   );
 }
 
 /** File read callback.
- * Responds to a SBP_MSG_FILEIO_READ message.
+ * Responds to a SBP_MSG_FILEIO_READ_REQUEST message.
  *
  * Reads a certain length (up to 255 bytes) from a given offset. Returns the
- * data in a SBP_MSG_FILEIO_READ message where the message length field indicates
- * how many bytes were succesfully read.
+ * data in a SBP_MSG_FILEIO_READ_RESPONSE message where the message length field
+ * indicates how many bytes were succesfully read.
  */
 static void read_cb(u16 sender_id, u8 len, u8 msg[], void* context)
 {
@@ -81,16 +81,16 @@ static void read_cb(u16 sender_id, u8 len, u8 msg[], void* context)
   len += cfs_read(f, buf + len, readlen);
   cfs_close(f);
 
-  sbp_send_msg(SBP_MSG_FILEIO_READ, len, buf);
+  sbp_send_msg(SBP_MSG_FILEIO_READ_RESPONSE, len, buf);
 }
 
 /** Directory listing callback.
- * Responds to a SBP_MSG_FILEIO_READ_DIR message.
+ * Responds to a SBP_MSG_FILEIO_READ_DIR_REQUEST message.
  *
  * The offset parameter can be used to skip the first n elements of the file
  * list.
  *
- * Returns a SBP_MSG_FILEIO_READ_DIR message containing the directory
+ * Returns a SBP_MSG_FILEIO_READ_DIR_RESPONSE message containing the directory
  * listings as a NULL delimited list. The listing is chunked over multiple SBP
  * packets and the end of the list is identified by an entry containing just
  * the character 0xFF.
@@ -124,7 +124,7 @@ static void read_dir_cb(u16 sender_id, u8 len, u8 msg[], void* context)
 
   cfs_closedir(&dir);
 
-  sbp_send_msg(SBP_MSG_FILEIO_READ_DIR, len, buf);
+  sbp_send_msg(SBP_MSG_FILEIO_READ_DIR_RESPONSE, len, buf);
 }
 
 /* Remove file callback.
@@ -144,10 +144,11 @@ static void remove_cb(u16 sender_id, u8 len, u8 msg[], void* context)
 }
 
 /* Write to file callback.
- * Responds to a SBP_MSG_FILEIO_WRITE message.
+ * Responds to a SBP_MSG_FILEIO_WRITE_REQUEST message.
  *
  * Writes a certain length (up to 255 bytes) at a given offset. Returns a copy
- * of the original SBP_MSG_FILEIO_WRITE message to check integrity of the write.
+ * of the original SBP_MSG_FILEIO_WRITE_RESPONSE message to check integrity of
+ * the write.
  */
 static void write_cb(u16 sender_id, u8 len, u8 msg[], void* context)
 {
@@ -166,5 +167,5 @@ static void write_cb(u16 sender_id, u8 len, u8 msg[], void* context)
   cfs_write(f, msg + headerlen, len - headerlen);
   cfs_close(f);
 
-  sbp_send_msg(SBP_MSG_FILEIO_WRITE, headerlen, msg);
+  sbp_send_msg(SBP_MSG_FILEIO_WRITE_RESPONSE, headerlen, msg);
 }
