06de1ff1876dbd918ac38c79f1e9d1505ce1f3a6
Ben Segal <ben@swift-nav.com>
2015-10-08 17:27:52 -0700

    Added heading calculation and SBP logging.

--- src/sbp_utils.c
+++ src/sbp_utils.c
@@ -146,6 +146,14 @@ void sbp_make_baseline_ned(msg_baseline_ned_t *baseline_ned, const gps_time_t *t
   baseline_ned->flags = flags;
 }
 
+void sbp_make_heading(msg_baseline_heading_t *baseline_heading, const gps_time_t *t, 
+                      const double heading, u8 n_sats, u8 flags) {
+    baseline_heading->tow = round(t->tow * 1e3);
+    baseline_heading->heading = (u32)round(heading * 1e3);
+    baseline_heading->n_sats = n_sats;
+    baseline_heading->flags = flags;
+}
+
 void unpack_obs_header(const observation_header_t *msg, gps_time_t* t, u8* total, u8* count)
 {
   t->tow = ((double)msg->t.tow) / MSG_OBS_TOW_MULTIPLIER;
