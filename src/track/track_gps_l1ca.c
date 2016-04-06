a879629fecbf4ee19897117b33c9f33e96c05ddc
Tommi Paakki <tpaakki@exafore.com>
2016-04-06 18:02:25 +0300

    Change cutoff frequency

--- src/track/track_gps_l1ca.c
+++ src/track/track_gps_l1ca.c
@@ -48,7 +48,7 @@
 #define LD_PARAMS_EXTRAOPT "0.02, 0.8, 150, 50"
 #define LD_PARAMS_DISABLE  "0.02, 1e-6, 1, 1"
 
-#define CN0_EST_LPF_CUTOFF 5
+#define CN0_EST_LPF_CUTOFF 0.1
 
 static struct loop_params {
   float code_bw, code_zeta, code_k, carr_to_code;
