5a6f6dba609a67bdd97f3c72931aaf2007a66ff4
Jacob McNamee <jacob@swiftnav.com>
2016-05-12 15:00:07 -0700

    Compute measurement time delta in tracking_channel_measurement_get()

--- src/solution.c
+++ src/solution.c
@@ -153,7 +153,7 @@ double calc_heading(const double b_ned[3])
  * \param flags u8 RTK solution flags. 1 if float, 0 if fixed
  */
 void solution_send_baseline(const gps_time_t *t, u8 n_sats, double b_ecef[3],
-                            double ref_ecef[3], u8 flags, double hdop, 
+                            double ref_ecef[3], u8 flags, double hdop,
                             double corrections_age, u16 sender_id)
 {
   double* base_station_pos;
@@ -411,12 +411,13 @@ static void solution_thread(void *arg)
       solution_simulation();
     }
 
+    u64 nav_tc = nap_timing_count();
     u8 n_ready = 0;
     channel_measurement_t meas[MAX_CHANNELS];
     for (u8 i=0; i<nap_track_n_channels; i++) {
       tracking_channel_lock(i);
       if (use_tracking_channel(i)) {
-        tracking_channel_measurement_get(i, &meas[n_ready]);
+        tracking_channel_measurement_get(i, nav_tc, &meas[n_ready]);
         n_ready++;
       }
       tracking_channel_unlock(i);
@@ -432,7 +433,6 @@ static void solution_thread(void *arg)
      * more intelligent with the solution time.
      */
     static u8 n_ready_old = 0;
-    u64 nav_tc = nap_timing_count();
     static navigation_measurement_t nav_meas[MAX_CHANNELS];
 
     const channel_measurement_t *p_meas[n_ready];
@@ -445,8 +445,7 @@ static void solution_thread(void *arg)
     }
 
     ephemeris_lock();
-    if (calc_navigation_measurement(n_ready, p_meas, p_nav_meas,
-                                    (double)((u32)nav_tc)/SAMPLE_FREQ, p_e_meas)
+    if (calc_navigation_measurement(n_ready, p_meas, p_nav_meas, p_e_meas)
         != 0) {
       log_error("calc_navigation_measurement() returned an error");
       ephemeris_unlock();
@@ -522,7 +521,7 @@ static void solution_thread(void *arg)
                                     sdiffs);
             ephemeris_unlock();
             if (num_sdiffs >= 4) {
-              output_baseline(num_sdiffs, sdiffs, &position_solution.time, pdt, 
+              output_baseline(num_sdiffs, sdiffs, &position_solution.time, pdt,
                               dops.hdop, base_obss.sender_id);
             }
           }
