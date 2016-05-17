e320b643a2d9c84987bda033f86a971d9ab184aa
Pasi Miettinen <pasi.miettinen@exafore.com>
2016-05-17 10:22:15 +0300

    Fix L2C reacquisition

--- src/track/track_gps_l2cm.c
+++ src/track/track_gps_l2cm.c
@@ -209,6 +209,14 @@ void do_l1ca_to_l2cm_handover(u32 sample_count,
 
   if (0 == (l2c_cpbl & ((u32)1 << (sat - 1)))) {
     log_info_sid(sid, "SV does not support L2C signal");
+  }
+
+  /* Prevent tracking_startup_fifo from being flooded with same satellite.
+   * After calling tracking_startup_request() it takes few seconds before
+   * channel is marked as active (after succesful tracker channel init) and
+   * during this time multiple handover requests arrive. */
+  if (!l1ca_l2cm_handover_reserve(sat)) {
+    /* handover already in progress */
     return;
   }
 
