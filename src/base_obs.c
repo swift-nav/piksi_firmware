8f327160d17cf2e37a1e214f90febb46dc6fa5e5
dzollo <dzollo@swift-nav.com>
2016-05-25 17:15:27 -0700

    remove premature lock_counter assignment.

--- src/base_obs.c
+++ src/base_obs.c
@@ -126,8 +126,6 @@ static void update_obss(obss_t *new_obss)
     double clock_err;
     double clock_rate_err;
 
-    /* Set the lock counter TODO: deprecate in favor of lock time */
-    new_obss->nm[i].lock_counter = 0; 
     /* Calculate satellite parameters using the ephemeris. */
     ephemeris_lock();
     ephemeris_t *e = ephemeris_get(new_obss->nm[i].sid);
